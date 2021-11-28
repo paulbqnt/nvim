b874689b31f9b9e6d23acc2f89b1b7b2d0368049
paulbqnt <paul.boquant@gmail.com>
2021-11-28 18:23:46 +0100

    update before nvim-cmp

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -2,7 +2,9 @@ local wk = require("which-key")
 local mappings = {
   q = {":q<cr>", "Quit"},
   x = {":bdelete<cr>", "Close"},
-  E = {":e ~/.config/nvim/init.lua", "Edit config"}
+  E = {":e ~/.config/nvim/init.lua<cr>", "Edit config"},
+  f = {":Telescope find_files<cr>", "Telescope Find Files"},
+  r = {":Telescope live_grep<cr>", "Telescope Live Grep"}
 }
 local opts = {prefix = '<leader>'}
 wk.register(mappings, opts)
